5930703668136881012bd8488e204b0d587d397d
Andyrewy7 <89996690+Andyrewy7@users.noreply.github.com>
2023-05-17 13:06:22 -0400

    Initial Commit

---
+++ Weather!/Weather_App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App.swift
+//  Weather!
+//
+//  Created by Andrew Yang on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
